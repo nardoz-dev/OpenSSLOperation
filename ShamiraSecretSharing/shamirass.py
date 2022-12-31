58273cd915c65a5e3e0a6e47ecd698dbd50016b3
nardoz-dev <nardomen96@gmail.com>
2022-12-31 13:43:46 +0100

    adding shamira secret sharing

---
+++ ShamiraSecretSharing/shamirass.py
@@ -0,0 +1,51 @@
+import random
+
+def define_secret_and_parameters(secret, n, k):
+    # Define the secret and the pair (n,k)
+    return secret, n, k
+
+def generate_fragments(secret, n, k):
+    # Generate the polynomial coefficients
+    coefficients = [secret] + [random.randint(0, secret) for i in range(k-1)]
+
+    # Generate the fragments
+    fragments = []
+    for i in range(1, n+1):
+        fragment = (i, sum(coefficient * i**exponent for exponent, coefficient in enumerate(coefficients)))
+        fragments.append(fragment)
+
+    return fragments
+
+def reconstruct_secret(fragments, k):
+    # Reconstruct the secret using Lagrange interpolation
+    secret = 0
+    for i, fragment in enumerate(fragments[:k]):
+        xi, yi = fragment
+        secret += yi * product(xj/(xj - xi) for j, (xj, _) in enumerate(fragments[:k]) if i != j)
+    return secret
+
+def product(iterable):
+    # Helper function to compute the product of a list of numbers
+    result = 1
+    for x in iterable:
+        result *= x
+    return result
+
+# Define the secret and the pair (n,k)
+secret, n, k = define_secret_and_parameters(12345, 7, 4)
+
+# Generate the fragments
+fragments = generate_fragments(secret, n, k)
+print(f"Generated fragments: {fragments}")
+
+# Reconstruct the secret using 3 fragments
+reconstructed_secret = reconstruct_secret(fragments[:4], 4)
+print(f"Reconstructed secret using 4 fragments: {reconstructed_secret}")
+
+# Reconstruct the secret using 4 fragments
+reconstructed_secret = reconstruct_secret(fragments[:5], 5)
+print(f"Reconstructed secret using 5 fragments: {reconstructed_secret}")
+
+# Reconstruct the secret using 2 fragments (should fail)
+reconstructed_secret = reconstruct_secret(fragments[:3], 3)
+print(f"Reconstructed secret using less fragments (should fail) : {reconstructed_secret}")
